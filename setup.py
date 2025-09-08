9897feab394a6a9300334743f062675176b808fc
David Brown <david.brown@inria.fr>
2025-09-08 11:54:32 +0200

    Rebase from 'release/humble/namosim'

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_requirements():
 
 setup(
     name=package_name,
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(
         exclude=["tests", "tests.*"]
     ),  # Exclude tests and subpackages
